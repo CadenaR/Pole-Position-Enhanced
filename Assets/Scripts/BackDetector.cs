523eb798f1092108a47574229fbc23c914fad5c0
nesherben <nestor8299@gmail.com>
2020-06-06 20:07:53 +0200

    marcha atras parte 1

---
+++ Assets/Scripts/BackDetector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackDetector : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "BackDetector") {
+
+            Debug.Log("esta marcha atras"); //flag aqui para el script de UI
+        
+        }
+    }
+}
